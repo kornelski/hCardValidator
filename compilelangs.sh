9cc846ef20d748ed32a7c3582c5833c8c6b4f719
porneL <pornel@pornel.net>
2010-10-31 21:12:55 +0000

    Script for compiling .po files

---
+++ compilelangs.sh
@@ -0,0 +1,8 @@
+msgfmt /www/hcard/locale/en/LC_MESSAGES/errors.po -o /www/hcard/locale/en/LC_MESSAGES/errors.mo
+msgfmt /www/hcard/locale/en/LC_MESSAGES/props.po  -o /www/hcard/locale/en/LC_MESSAGES/props.mo
+msgfmt /www/hcard/locale/en/LC_MESSAGES/main.po   -o /www/hcard/locale/en/LC_MESSAGES/main.mo
+msgfmt /www/hcard/locale/pl/LC_MESSAGES/errors.po -o /www/hcard/locale/pl/LC_MESSAGES/errors.mo
+msgfmt /www/hcard/locale/pl/LC_MESSAGES/props.po  -o /www/hcard/locale/pl/LC_MESSAGES/props.mo
+msgfmt /www/hcard/locale/pl/LC_MESSAGES/main.po   -o /www/hcard/locale/pl/LC_MESSAGES/main.mo
+
+#sudo /usr/local/apache/sbin/apachectl restart
